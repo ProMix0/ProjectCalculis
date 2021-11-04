550e3078c8d2646794ca808806773e1da583fe96
ProMix0 <misha.zavertkin@mail.ru>
2021-11-04 22:27:33 +0700

    Client DI

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,19 +1,28 @@
 ﻿using MainLibrary.Classes;
 using MainLibrary.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System;
 using System.Net;
 using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Client
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            IRemoteServer server = new RemoteServer();
-            server.ConnectTo(new(IPAddress.Loopback, 8008));
-            IWork work = server.GetWorkAsync("TestWork").Result;
-            work.Execute(new object[] { Array.Empty<string>() });
+            Console.WriteLine(BitConverter.ToInt32(Encoding.UTF8.GetBytes("0тd ")));
+            foreach (byte b in Encoding.UTF8.GetBytes(@"Ї  "))
+                Console.WriteLine(b);
+            await new HostBuilder()
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .RunConsoleAsync();
         }
     }
 }
---
+++ Client/Worker.cs
@@ -0,0 +1,30 @@
+﻿using MainLibrary.Classes;
+using MainLibrary.Interfaces;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Worker : IHostedService
+    {
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            IRemoteServer server = new RemoteServer();
+            server.ConnectTo(new(IPAddress.Loopback, 8008));
+            IWork work = await server.GetWorkAsync("TestWork");
+            await work.Execute(new object[] { Array.Empty<string>() });
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+            //throw new NotImplementedException();
+        }
+    }
+}
