eb5cd9c7df5b6d6bebcdca358de24aafd531e409
ProMix0 <misha.zavertkin@mail.ru>
2021-11-10 21:56:51 +0700

    Client configuration

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,12 +1,16 @@
 ﻿using MainLibrary.Classes;
 using MainLibrary.Interfaces;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Environment;
 
 namespace Client
 {
@@ -15,6 +19,13 @@ namespace Client
         static async Task Main(string[] args)
         {
             await new HostBuilder()
+                .ConfigureAppConfiguration(configHost =>
+                {
+                    configHost.SetBasePath(Directory.GetCurrentDirectory());
+                    //configHost.AddInMemoryCollection(new Dictionary<string, string>() { { } });
+                    configHost.AddJsonFile(GetFolderPath(SpecialFolder.ApplicationData)+@"\ProjectCalculis\settings.json", optional: true);
+                    configHost.AddCommandLine(args);
+                })
                 .ConfigureServices(services =>
                 {
                     services.AddHostedService<Worker>();
