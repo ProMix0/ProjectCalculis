9c7b7319830ad8dcdde8d9494211d08edc57efa0
ProMix0 <misha.zavertkin@mail.ru>
2021-11-22 02:03:27 +0700

    LifeWork draft

---
+++ LifeWork/ClientWork.cs
@@ -0,0 +1,14 @@
+﻿using MainLibrary.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace LifeWork
+{
+    public class ClientWork : IClientCode
+    {
+        public Task<byte[]> Entrypoint(byte[] args)
+        {
+            return Task.FromResult(new byte[] { 1, 2, 3 });
+        }
+    }
+}
---
+++ LifeWork/ServerWork.cs
@@ -0,0 +1,17 @@
+﻿using MainLibrary.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifeWork
+{
+    class ServerWork : IServerCode
+    {
+        public byte[] GetArgument()
+        {
+            return new byte[] { 3, 2, 1 };
+        }
+    }
+}
